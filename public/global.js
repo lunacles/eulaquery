5e5cfd2262c6fd5e943a7cfbe88097545da53ad5
Damocles <lunahackley@gmail.com>
2023-12-21 00:48:49 -0500

    Create global.js

---
+++ public/global.js
@@ -0,0 +1,28 @@
+const global = {
+    canvas: null,
+    api: {
+      url: 'https://api.rule34.xxx/',
+      posts: 'page=dapi&s=post&q=index',
+      limit: '&limit=10',
+      activeTags: [],
+    },
+    font: {
+      family: 'Ubuntu',
+      style: 'bold',
+      size: 16,
+    },
+    colors: {
+      pureBlack: '#000000',
+      pureWhite: '#ffffff',
+      white: '#f6f6f6',
+      black: '#0e0e0e',
+      bgBlack: '#202027',
+
+      lightBlue: '#b2dee6',
+      burple: '#5f66c4',
+      navyBlue: '#434879',
+      emperor: '#4e4447',
+      darkGray: '#333747',
+    }
+  }
+  export default global
