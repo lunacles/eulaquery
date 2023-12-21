db69724b1c8ff230c293b8db1e6852dd22f96744
Damocles <lunahackley@gmail.com>
2023-12-21 00:54:31 -0500

    Create document.js

---
+++ public/document.js
@@ -0,0 +1,27 @@
+import { Element } from './elements.js'
+import { mouse, keyboard } from './utilities/event.js'
+
+const Document = {
+  // TODO: Improve how this is done lol
+  e: new Element(),
+  get width() {
+    return window.innerWidth
+  },
+  get height() {
+    return window.innerHeight
+  },
+  get centerX() {
+    return Document.width * 0.5
+  },
+  get centerY() {
+    return Document.height * 0.5
+  },
+  refreshCanvas() {
+    this.e.resize({ width: window.innerWidth, height: window.innerHeight, scale: 1 })
+
+    mouse.doubleClick = false
+    keyboard.e = null
+  },
+}
+
+export default Document
