7bd19b74a139edbb3a5b8088ef40fede958ae8a3
Damocles <lunahackley@gmail.com>
2023-12-21 21:41:32 -0500

    Fix bug & adjust ClickRegion update

--- public/components/tag.js
+++ public/components/tag.js
@@ -2,9 +2,11 @@ import global from '../global.js'
 import {
   Element,
   Text,
+  Rect,
 } from '../elements.js'
 import Bar from './bar.js'
-import ClickRegion from './clickRegion.js'
+import Interpolator from './interpolation.js'
+import ClickRegion from './clickregion.js'
 
 import { mouse } from '../utilities/event.js'
 
@@ -22,22 +24,23 @@ const Tag = class extends Element {
   constructor(label, type) {
     super()
     this.label = label
-    // TODO: Implement this
     this.type = type
 
     this.width = 0
 
     this.clickRegion = ClickRegion.create()
+    this.interpolation = Interpolator.create({ speed: 2, sharpness: 6 })
   }
-  /**
+ /**
    * Draws the tag display onto the canvas and checks for clicks.
    * @public
-   * @param {Number} x - The x-coordinate of the tag.
-   * @param {Number} y - The y-coordinate of the tag.
-   * @param {Number} size - The size of the tag.
+   * @param {Number} x - The x-coordinate of the tag display.
+   * @param {Number} y - The y-coordinate of the tag display.
+   * @param {Number} size - The size of the tag display.
    */
   draw({ x = 0, y = 0, size = 0 }) {
-    let width = this.measureText(this.label, size).width
+    let label = this.label.length <= 30 ? this.label : this.label.slice(0, 30) + "..."
+    let width = this.measureText(label, size).width
     this.width = width + size
     Bar.draw({
       x: x - width * 0.5, y: y - size * 0.5,
@@ -46,21 +49,18 @@ const Tag = class extends Element {
     Text.draw({
       x, y: y - size * 0.25,
       size: size * 0.8,
-      text: this.label,
+      text: label,
       align: 'center',
     }).fill(global.colors.white)
 
     this.clickRegion.update({
-      x: x - width * 0.5, y: y - size * 0.5,
-      width, height: size
+      x: x - width * 0.5 - size * 0.5, y: y - size,
+      width: width + size, height: size
     })
 
     if (this.clickRegion.check() && mouse.left) {
       let index = global.api.activeTags.indexOf(this.label)
-      console.log(global.api.activeTags)
-
       global.api.activeTags.splice(index, 1)
-      console.log(global.api.activeTags)
     }
   }
 }
