42cb32f1d53ed32eccd36346c140a8e649c3507b
Damocles <lunahackley@gmail.com>
2023-12-22 10:10:43 -0500

    Update Bar directory & modularize snowfall

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import {
   Rect,
-  Circle,
   Text,
+  Bar,
 } from './public/elements.js'
 import Input from './public/components/input.js'
 import AutoCompleteResults from './public/components/autocomplete.js'
-import Bar from './public/components/bar.js'
 import Media from './public/components/media.js'
 import Interpolator from './public/components/interpolation.js'
 import TagContainer from './public/components/tagcontainer.js'
+import Snowfall from './public/components/snowfall.js'
 
 import global from './public/global.js'
 import Document from './public/document.js'
-//import { Page } from './src/api/post.js'
 
 import * as util from './public/utilities/util.js'
 
@@ -25,8 +24,7 @@ let icon = Media.image('./grimheart.svg')
 
 let tagContainer = TagContainer.create(20)
 
-//let testVideo = Post.getPostById(4788546).then(video => Media.video(video.fileUrl))
-//let page = Page.get({ page: 0, tags: ['eula_(genshin_impact)'] })
+let snow = new Snowfall(50)
 
 const placeholder = {
   spacing: 5,
@@ -34,58 +32,6 @@ const placeholder = {
   searchBarWidth: 400,
   searchBarHeight: 50,
 }
-
-// Refactored someone else's code cuz im lazy lol
-const Snowfall = class {
-  constructor(max) {
-    this.max = max
-    this.particles = Array(max).fill().map(() => ({
-      x: Math.random() * Document.width,
-      y: Math.random() * Document.height,
-      r: Math.random() * 8 + 1,
-      d: Math.random() * this.max
-    }))
-
-    this.angle = 0
-  }
-  draw() {
-    for (let particle of this.particles) {
-      Circle.draw({
-        x: particle.x, y: particle.y, radius: particle.r
-      }).alpha(0.98).fill(global.colors.white)
-    }
-    this.update()
-  }
-  update() {
-    this.angle += 0.005
-    for (let [i, particle] of this.particles.entries()) {
-      particle.x += Math.sin(this.angle) * 2
-      particle.y += Math.cos(this.angle + particle.d) + 1 + particle.r / 10
-
-      if (particle.x > Document.width + 5 || particle.x < -5 || particle.y > Document.height) {
-        if (i % 3 > 0) {
-          this.particles[i] = {
-            x: Math.random() * Document.width, y: -10,
-            r: particle.r, d: particle.d
-          }
-        } else {
-          if (Math.sin(this.angle) > 0) {
-            this.particles[i] = {
-              x: -5, y: Math.random() * Document.height,
-              r: particle.r, d: particle.d
-            }
-          } else {
-            this.particles[i] = {
-              x: Document.width + 5, y: Math.random() * Document.height,
-              r: particle.r, d: particle.d
-            }
-          }
-        }
-      }
-    }
-  }
-}
-let snow = new Snowfall(50)
 
 const UI = {
   background() {
@@ -169,15 +115,6 @@ const UI = {
       spacing: 10,
     })
   },
-  backgroundShapes() {
-    let width = (placeholder.searchBarWidth + placeholder.searchBarHeight) * 1.1
-    //RoundRect.draw({
-    //  x: Document.centerX - width * 0.5, y: 0,
-    //  width, height: placeholder.titleSize * 1.3 + placeholder.spacing,
-    //  radii: [0, 0, 25, 25],
-    //}).alpha(0.5).fill(global.colors.navyBlue)
-
-  }
 }
 
 let time = 0
