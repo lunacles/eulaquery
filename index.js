1596b2984c4c332f16f5ce1cb7e38596d0e626f3
Damocles <lunahackley@gmail.com>
2023-12-23 21:00:43 -0500

    Rewrote UI object into class & optimize mobile

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import global from './public/global.js'
+import Document from './public/document.js'
+
 import {
   Rect,
   Text,
@@ -8,54 +11,84 @@ import AutoCompleteResults from './public/components/autocomplete.js'
 import Media from './public/components/media.js'
 import Interpolator from './public/components/interpolation.js'
 import TagContainer from './public/components/tagcontainer.js'
+import SearchButton from './public/components/searchbutton.js'
+import SearchResults from './public/components/searchresults.js'
 import Snowfall from './public/components/snowfall.js'
-
-import global from './public/global.js'
-import Document from './public/document.js'
 
 import * as util from './public/utilities/util.js'
 
 let searchBar = Input.create({ onEnter: () => {}, maxLength: 107 })
 let searchBarResults = AutoCompleteResults.create({ hook: searchBar })
+let searchButton = SearchButton.create({ hook: searchBar })
+let searchResults = SearchResults.create()
 
 let loadingFade = Interpolator.create({ speed: 1, sharpness: 2 })
 loadingFade.set(0)
-let icon = Media.image('./grimheart.svg')
+let icon = Media.image('./grimheart.svg', true)
 
-let tagContainer = TagContainer.create(20)
-
+let tagContainer = TagContainer.create()
 let snow = new Snowfall(50)
 
-const placeholder = {
-  spacing: 5,
-  titleSize: 75,
-  searchBarWidth: 400,
-  searchBarHeight: 50,
-}
+const UI = class {
+  constructor() {
+    this.spacing = 5
 
-const UI = {
+    this.grimheartSize = Document.height * 0.75
+    this.titleSize = 75
+    this.searchBarWidth = Document.width * 0.35
+    this.searchBarHeight = 50
+
+    this.tagContainerHeight = 0
+  }
+  get ratio() {
+    return Document.width / Document.height
+  }
+  get vertical() {
+    return Document.width / Document.height < 1
+  }
+  render() {
+    if (Document.width / Document.height > 1) {
+      // PC
+      this.grimheartSize = Document.height * 0.75
+      this.titleSize = 75
+      this.searchBarWidth = Document.width * 0.35
+      this.searchBarHeight = 50
+
+    } else {
+      // Mobile
+      this.grimheartSize = Document.width
+      this.titleSize = 125
+      this.searchBarWidth = Document.width * 0.75
+      this.searchBarHeight = 100
+    }
+    this.background()
+    this.snowfall()
+    this.grimheartIcon()
+    this.radialGradient()
+    this.title()
+    this.activeTags()
+    this.searchResults()
+    this.searchBar(time)
+  }
   background() {
     Rect.draw({
       x: 0, y: 0,
-      width: Document.width * 2, height: Document.height * 2,
+      width: Document.width, height: Document.height,
     }).fill(global.colors.bgBlack)
-  },
+  }
   snowfall() {
     snow.draw()
-  },
+  }
   grimheartIcon() {
     if (icon.loaded) {
       loadingFade.set(1)
-      let size = Document.height * 0.75
-      if (Document.width < Document.height)
-        size = Document.width
 
       icon.alpha(Math.max(0, loadingFade.get() - 0.3)).draw({
-        x: Document.centerX - size * 0.5, y: Document.centerY - size * 0.375,
-        width: size, height: size
+        x: Document.centerX - this.grimheartSize * 0.5, y: Document.centerY - this.grimheartSize * 0.375,
+        width: this.grimheartSize, height: this.grimheartSize
       })
     }
-  },
+  }
   radialGradient() {
     Rect.draw({
       x: 0, y: 0,
@@ -65,34 +98,34 @@ const UI = {
       x2: Document.centerX, y2: Document.height * 2, r2: 0,
       gradient: [{ color: global.colors.bgBlack, pos: 0.5, }, { color: util.mixColors(global.colors.white, global.colors.navyBlue, 0.99), pos: 1 }]
     })
-  },
+  }
   title() {
-    let eulaWidth = util.measureText('Eula', 50).width * 2
-    let queryWidth = util.measureText('query', 50).width * 2
+    let eulaWidth = util.measureText('Eula', this.titleSize).width * 2
+    let queryWidth = util.measureText('query', this.titleSize).width * 2
     let offset = (eulaWidth - queryWidth) * 0.25
     Text.draw({
-      x: Document.centerX + offset, y: placeholder.titleSize,
-      size: placeholder.titleSize,
+      x: Document.centerX + offset, y: this.titleSize,
+      size: this.titleSize,
       text: 'Eula',
       align: 'right',
     }).fill(global.colors.lightBlue)
     Text.draw({
-      x: Document.centerX + offset, y: placeholder.titleSize,
-      size: placeholder.titleSize,
+      x: Document.centerX + offset, y: this.titleSize,
+      size: this.titleSize,
       text: 'query',
       align: 'left'
     }).fill(global.colors.burple)
-  },
+  }
   searchBar(t) {
     let padding = 10
-    let width = placeholder.searchBarWidth
-    let height = placeholder.searchBarHeight
+    let width = this.searchBarWidth
+    let height = this.searchBarHeight
     let x = Document.centerX - width * 0.5
-    let y = placeholder.titleSize * 2 + placeholder.spacing
+    let y = this.titleSize * 2 + this.spacing
 
     searchBarResults.draw({
       x, y: y + height * 0.25,
-      width: width + padding, height: height * 0.25 + padding,
+      width: width + padding, height: height * 0.35 + padding,
       t: time,
     })
     Bar.draw({
@@ -100,35 +133,47 @@ const UI = {
       width: width + padding, height: height + padding,
     }).fill(global.colors.darkGray)
     searchBar.draw({
-      x: x + width * 0.5 - padding * 0.5, y,
-      width: width + padding * 2, height: height * 0.8, padding,
+      x: x + width * 0.5 * 0.9 - padding * 0.5, y,
+      width: width * 0.9 + padding * 2, height: height * 0.8, padding,
       t,
     })
-  },
-  activeTags() {
-    let x = Document.centerX - (placeholder.searchBarWidth + placeholder.searchBarHeight + 20) * 0.5
-    let y = placeholder.titleSize * 2 + placeholder.searchBarHeight * 0.25 - 20 + placeholder.spacing
 
-    tagContainer.draw({
+    searchButton.draw({
+      x: Document.centerX + width * 0.5, y,
+      radius: this.vertical ? height * 0.9 : height,
+      offset: padding,
+    })
+  }
+  activeTags() {
+    let x = Document.centerX - (this.searchBarWidth + this.searchBarHeight + 20) * 0.5
+    let y = this.titleSize * 2 + this.searchBarHeight * 0.25 - 20 + this.spacing
+
+    this.tagContainerHeight = tagContainer.draw({
       x, y,
-      width: placeholder.searchBarWidth + placeholder.searchBarHeight + 20, heightOffset: placeholder.searchBarHeight * 0.25 + 20,
+      width: this.searchBarWidth + this.searchBarHeight + 20, heightOffset: this.searchBarHeight * 0.25 + 20,
+      tagSize: this.searchBarHeight * 0.4,
       spacing: 10,
     })
-  },
+  }
+  searchResults() {
+    let width = Math.min(this.searchBarWidth * 1.25, Document.width)
+    let x = Document.centerX - width * 0.5
+    let y = this.titleSize * 2 + this.searchBarHeight * 0.25 - 20 + this.spacing * 4 + this.tagContainerHeight
+    searchResults.draw({
+      x, y,
+      width
+    })
+  }
 }
+
+const ui = new UI()
 
 let time = 0
 let appLoop = async (newTime) => {
   let timeElapsed = newTime - time
   time = newTime
 
-  UI.background()
-  UI.snowfall()
-  UI.grimheartIcon()
-  UI.radialGradient()
-  UI.title()
-  UI.activeTags()
-  UI.searchBar(time)
+  ui.render()
 
   Document.refreshCanvas()
   requestAnimationFrame(appLoop)
