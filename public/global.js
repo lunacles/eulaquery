d652099afd64d33d18fac0566702f5495dce37ee
Damocles <lunahackley@gmail.com>
2023-12-23 21:01:45 -0500

    Remove useless variable

--- public/global.js
+++ public/global.js
@@ -1,28 +1,34 @@
 const global = {
-    canvas: null,
-    api: {
-      url: 'https://api.rule34.xxx/',
-      posts: 'page=dapi&s=post&q=index',
-      limit: '&limit=10',
-      activeTags: [],
-    },
-    font: {
-      family: 'Ubuntu',
-      style: 'bold',
-      size: 16,
-    },
-    colors: {
-      pureBlack: '#000000',
-      pureWhite: '#ffffff',
-      white: '#f6f6f6',
-      black: '#0e0e0e',
-      bgBlack: '#202027',
+  title: '',
+  canvas: null,
+  ctx: null,
+  scale: 1,
+  clickOverride: false,
+  api: {
+    url: 'https://api.rule34.xxx/',
+    limit: 10,
+    activeTags: [],
+    page: 0,
+    results: null,
+  },
+  font: {
+    family: 'Ubuntu',
+    style: 'bold',
+    size: 16,
+  },
+  colors: {
+    pureBlack: '#000000',
+    pureWhite: '#ffffff',
+    white: '#f6f6f6',
+    black: '#0e0e0e',
+    bgBlack: '#202027',
 
-      lightBlue: '#b2dee6',
-      burple: '#5f66c4',
-      navyBlue: '#434879',
-      emperor: '#4e4447',
-      darkGray: '#333747',
-    }
+    lightBlue: '#b2dee6',
+    burple: '#5f66c4',
+    navyBlue: '#434879',
+    emperor: '#4e4447',
+    darkGray: '#333747',
   }
-  export default global
+}
+
+export default global
