cb8c4e9fd7f3e6c0c9cc8da510590aa781ff6177
Damocles <lunahackley@gmail.com>
2023-12-23 21:06:41 -0500

    Add interpolation

--- public/components/tagcontainer.js
+++ public/components/tagcontainer.js
@@ -6,18 +6,18 @@ import {
 } from '../elements.js'
 
 import Interpolator from './interpolation.js'
+import AutoCompleteResults from './autocomplete.js'
 
 const TagContainer = class extends Element {
   /**
    * Creates a new TagContainer.
    * @public
-   * @param {Number} tagSize - The size of the tag displays.
    * @returns {TagContainer} The current instance.
    */
-  static create(tagSize) {
-    return new TagContainer(tagSize)
+  static create() {
+    return new TagContainer()
   }
-  constructor(tagSize) {
+  constructor() {
     super()
 
     this.x = 0
@@ -25,12 +25,13 @@ const TagContainer = class extends Element {
     this.width = 0
     this.heightOffset = 0
     this.maxRowLength = 0
-    this.tagSize = tagSize
+    this.tagSize = 0
 
-    this.interpolator = Interpolator.create({ speed: 0.4, sharpness: 6, })
+    this.interpolation = Interpolator.create({ speed: 0.7, sharpness: 6, })
 
-    for (let tag of global.api.activeTags)
+    for (let tag of global.api.activeTags) {
       tag.width = this.measureText(tag.label, tagSize).width + tagSize
+    }
   }
   /**
    * Draws the tag container and the tags within onto the canvas.
@@ -41,12 +42,13 @@ const TagContainer = class extends Element {
    * @param {Number} heightOffset - The height offset applied to tag displays.
    * @param {Number} spacing - The spacing between each tag display.
    */
-  draw({ x = 0, y = 0, width = 0, heightOffset = 0, spacing = 0, }) {
+  draw({ x = 0, y = 0, width = 0, heightOffset = 0, tagSize = 0, spacing = 0, }) {
     this.x = x
     this.y = y
     this.width = width
     this.heightOffset = heightOffset
-    this.maxRowLength = width * 0.8 - spacing
+    this.maxRowLength = this.width - spacing
+    this.tagSize = tagSize
 
     //if (global.api.activeTags.length <= 0) return
     let rows = [[]]
@@ -54,34 +56,57 @@ const TagContainer = class extends Element {
     let row = 0
     for (let tag of global.api.activeTags) {
       if (rowLength + spacing + tag.width + this.tagSize <= this.maxRowLength) {
-        rowLength += spacing + tag.width + this.tagSize
         rows[row].push(tag)
+        rowLength += spacing + tag.width + this.tagSize
       } else {
         row++
-        rowLength = 0
+        rowLength = spacing + tag.width + this.tagSize
         rows.push([tag])
       }
     }
 
-    this.interpolator.set(rows.length)
+    this.interpolation.set(rows.length)
     RoundRect.draw({
       x, y,
-      width, height: this.tagSize * this.interpolator.get() + spacing * (this.interpolator.get() + 1) + this.heightOffset,
+      width, height: this.tagSize * this.interpolation.get() + spacing * (this.interpolation.get() + 1) + this.heightOffset,
       radii: [0, 0, 30, 30]
     }).fill(global.colors.navyBlue)
 
-    for (let [i, row] of rows.entries()) {
-      let rowLength = row.reduce((a, b) => a + b.width, 0) + (row.length - 1) * spacing
+    let i = 0
+    for (let row of rows) {
+      let rowLength = row.filter(r => r.active).reduce((a, b) => a + b.width, 0) + (row.length - 1) * spacing
       let tagX = this.x + this.width * 0.5 - rowLength * 0.5
-      for (let [ii, tag] of row.entries()) {
+      let ii = 0
+      for (let tag of row) {
+        if (tag.interpolationX.get() < this.x)
+          tag.interpolationX.forceDisplay(this.x + this.width * 0.5)
+        if (tag.interpolationY.get() < this.y)
+          tag.interpolationY.forceDisplay(this.y)
+
+        if (!tag.active) {
+          tag.interpolationY.set(this.y)
+          if (tag.interpolationY.get() < this.y + this.heightOffset) {
+            let index = global.api.activeTags.findIndex(r => r.label === tag.label)
+            global.api.activeTags.splice(index, 1)
+          }
+        } else {
+          ii++
+          let targetX = tagX + tag.width * 0.5
+          tag.interpolationX.set(targetX)
+          tagX += tag.width + spacing
+
+          let targetY = this.y + (this.heightOffset + spacing * (i + 1) + this.tagSize * (i + 1))
+          tag.interpolationY.set(targetY)
+        }
+
         tag.draw({
-          x: tagX + tag.width * 0.5 + spacing * Math.min(0, ii), y: y + this.heightOffset + spacing * (i + 1) + this.tagSize * (i + 1),
+          x: tag.interpolationX.get(), y: tag.interpolationY.get(),
           size: this.tagSize,
         })
-        tagX += tag.width + spacing
-
       }
+      i++
     }
+    return this.tagSize * this.interpolation.get() + spacing * (this.interpolation.get() + 1) + this.heightOffset
   }
 }
 
