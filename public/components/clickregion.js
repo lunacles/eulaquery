f184283325686d9f9c5111431242e35f4537441f
Damocles <lunahackley@gmail.com>
2023-12-26 15:42:46 -0500

    Remove jsDoc

--- public/components/clickregion.js
+++ public/components/clickregion.js
@@ -5,15 +5,6 @@ import {
 } from '../elements.js'
 
 const ClickRegion = class {
-  /**
-   * Creates a new ClickRegion instance and applies the provided dimensions.
-   * @public
-   * @param {Number} x - The x-coordinate of the region.
-   * @param {Number} y - The y-coordinate of the region.
-   * @param {Number} width - The width of the region.
-   * @param {Number} height - The height of the region.
-   * @returns {ClickRegion} The current instance.
-   */
   static create({ x = 0, y = 0, width = 0, height = 0 } = {}) {
     return new ClickRegion(x, y, width, height)
   }
@@ -25,14 +16,6 @@ const ClickRegion = class {
 
     this.active = true
   }
-  /**
-   * Updates the ClickRegion position and dimensions.
-   * @public
-   * @param {Number} x - The new x-coordinate of the region.
-   * @param {Number} y - The new y-coordinate of the region.
-   * @param {Number} width - The new width of the region.
-   * @param {Number} height - The new height of the region.
-   */
   update({ x = 0, y = 0, width = 0, height = 0, debug = false } = {}) {
     this.x = x
     this.y = y
@@ -46,20 +29,10 @@ const ClickRegion = class {
       }).stroke('#ff0000', 1)
     }
   }
-  /**
-   * Checks the mouse position relative to the region boundaries.
-   * @public
-   * @returns {Bool} Whether the mouse is currently within or not within the provided boundaries.
-   */
   check() {
     if (!this.active) return false
     return mouse.x >= this.x && mouse.x < this.x + this.width && mouse.y >= this.y && mouse.y < this.y + this.height
   }
-  /**
-   * Toggles whether the ClickRegion will accept checks or not.
-   * @public
-   * @param {Bool} state - The toggle state.
-   */
   toggle(state) {
     this.active = state
   }
