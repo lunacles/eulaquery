2d5768e6365140a75cb4abc1fd4935d4f0ddfed5
Damocles <lunahackley@gmail.com>
2023-12-26 15:42:08 -0500

    Remove jsDoc & improve curve formula

--- public/components/interpolation.js
+++ public/components/interpolation.js
@@ -2,24 +2,9 @@ import { Element, } from '../elements.js'
 import * as util from '../utilities/util.js'
 
 const Interpolator = class extends Element {
-  /**
-   * Creates a new interpolation instance and applies a speed and sharpness to it.
-   * @public
-   * @param {Number} speed - The speed of the transition.
-   * @param {Number} sharpness - How sharp the translation is.
-   * @returns {Interpolator} The current instance.
-   */
   static create({ speed, sharpness }) {
     return new Interpolator(0, speed, sharpness)
   }
-  /**
-   * Creates a new interpolation instance and applies a speed and sharpness to it.
-   * @public
-   * @param {Number} size - The amount of interpolation instances returned.
-   * @param {Number} speed - The speed of the transition.
-   * @param {Number} sharpness - How sharp the translation is.
-   * @returns {Array<Interpolator>} An array of the instances.
-   */
   static createGroup({ size, speed, sharpness }) {
     return new Array(size).fill(new Interpolator(0, speed, sharpness))
   }
@@ -34,21 +19,11 @@ const Interpolator = class extends Element {
     this.old = interpolation
     this.frozen = false
   }
-  /**
-   * Freezes the interpolation for a given period.
-   * @public
-   * @param {Number} time - How long in milliseconds that the interpolation is for.
-   */
   async freeze(time) {
     this.frozen = true
     await util.sleep(time)
     this.frozen = false
   }
-  /**
-   * Sets the goal value of the interpolation.
-   * @public
-   * @param {Number} value - The numeric goal to transition to.
-   */
   set(value) {
     if (this.interpolation !== value && !this.frozen) {
       this.old = this.get()
@@ -57,14 +32,9 @@ const Interpolator = class extends Element {
     }
     return this
   }
-  /**
-   * Returns the current transition progress of the interpolation
-   * @public
-   * @returns {Number} The current value of the transition.
-   */
   get() {
     let seconds = (Date.now() - this.time) / 1000
-    let curve = 1 / (1 + Math.exp(-(((seconds / this.speed) * 2 - 1) * this.sharpness)))
+    let curve = 1 - Math.pow(1 - seconds / this.speed, this.sharpness)
 
     if (seconds >= this.speed) {
       this.display = this.interpolation
@@ -74,11 +44,6 @@ const Interpolator = class extends Element {
 
     return this.display
   }
-  /**
-   * Forces the display into a given value.
-   * @public
-   * @param {Number} value - The forced value.
-   */
   forceDisplay(value) {
     this.old = value
     this.display = value
