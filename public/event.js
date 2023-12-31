b1d0b9274aceefbaf5844560408b2834e53133bc
damocles <lunahackley@gmail.com>
2023-12-31 16:47:02 -0600

    Improve mobile support

--- public/event.js
+++ public/event.js
@@ -1,4 +1,8 @@
 import global from './global.js'
+import Document from './document.js'
+
+let touchStartY = 0
+let currentTouchY = 0
 
 export let mouse = {
   x: 0,
@@ -15,55 +19,70 @@ export let keyboard = {
   e: null
 }
 
-canvas.addEventListener('click', e => {
-  switch (e.button) {
-    case 0:
-      mouse.left = true
-      break
-    case 2:
-      mouse.right = true
-      break
+if (window.innerWidth / window.innerHeight > 1) {
+  canvas.addEventListener('click', e => {
+    switch (e.button) {
+      case 0:
+        mouse.left = true
+        break
+      case 2:
+        mouse.right = true
+        break
+    }
+  })
+  canvas.addEventListener('mousedown', e => {
+    mouse.held = true
+  })
+  canvas.addEventListener('mouseup', e => {
+    mouse.held = false
+  })
+  canvas.addEventListener('contextmenu', e => {
+    e.preventDefault()
+    mouse.right = true
+  })
+
+  canvas.addEventListener("dblclick", () => {
+    mouse.doubleClick = true
+  })
+
+  canvas.addEventListener('mousemove', e => {
+    mouse.x = e.clientX
+    mouse.y = e.clientY
+  })
+  canvas.addEventListener('wheel', e => {
+    e.preventDefault()
+    mouse.targetScroll -= Math.sign(e.deltaY) * 15
+  })
+} else {
+  let scroll = e => {
+    touchStartY = e.touches[0].clientY
+    let distance = touchStartY - currentTouchY
+    mouse.targetScroll -= Math.abs(distance)
   }
-})
-
-canvas.addEventListener('mousedown', e => {
-  mouse.held = true
-})
-canvas.addEventListener('mouseup', e => {
-  mouse.held = false
-})
-
-canvas.addEventListener('contextmenu', e => {
-  e.preventDefault()
-  mouse.right = true
-})
-
-canvas.addEventListener("dblclick", () => {
-  mouse.doubleClick = true
-})
-
-canvas.addEventListener('mousemove', e => {
-  mouse.x = e.clientX
-  mouse.y = e.clientY
-})
-
-canvas.addEventListener('wheel', e => {
-  e.preventDefault()
-  mouse.targetScroll -= Math.sign(e.deltaY) * 15
-})
-
-canvas.addEventListener('touchstart', e => {
-  mouse.left = true
-  mouse.x = e.touches[0].clientX
-  mouse.y = e.touches[0].clientY
-})
-canvas.addEventListener('touchend', () => {
-  mouse.left = false
-})
-canvas.addEventListener('touchmove', e => {
-  mouse.x = e.touches[0].clientX
-  mouse.y = e.touches[0].clientY
-})
+  canvas.addEventListener('touchstart', e => {
+    e.preventDefault()
+    mouse.left = true
+    mouse.held = true
+    mouse.x = e.touches[0].clientX
+    mouse.y = e.touches[0].clientY
+    mouse.touchStartY = e.touches[0].clientY
+  })
+  canvas.addEventListener('touchcancel', e => {
+    mouse.left = false
+  })
+  canvas.addEventListener('touchend', e => {
+    mouse.held = false
+    currentTouchY = e.changedTouches[0].clientY
+  })
+  canvas.addEventListener('touchmove', e => {
+    currentTouchY = e.touches[0].clientY
+    mouse.x = e.touches[0].clientX
+    mouse.y = e.touches[0].clientY
+    let deltaY = touchStartY - currentTouchY
+    mouse.targetScroll -= Math.sign(deltaY) * 4
+    touchStartY = currentTouchY
+  })
+}
 
 window.addEventListener('keydown', e => {
   keyboard.e = e
