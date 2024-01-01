0d8f528fb4ba52109de42ba92fb202df46353614
damocles <lunahackley@gmail.com>
2024-01-01 16:20:29 -0600

    Add profiler & allow api server changes

--- public/processor.js
+++ public/processor.js
@@ -1,16 +1,25 @@
+import Profiler from './profiler.js'
+import global from './global.js'
+
 // Fuck CORS
 const processor = async (src) => {
+  Profiler.logs.processor.set()
   try {
     let response = await fetch('https://eulaquery.glitch.me', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ link: src })
+      body: JSON.stringify({ link: src.replace(/api-cdn(-mp4)?/g, global.api.server) })
     })
     let blob = await response.blob()
     let url = URL.createObjectURL(blob)
     let buffer = await blob.arrayBuffer().then(arrayBuffer => new Uint8Array(arrayBuffer))
+    Profiler.logs.processor.mark()
+
+    if (global.debug)
+      console.log('Processor time:', `${Profiler.logs.processor.sum()}ms`)
+
     return { url, buffer }
   } catch (err) {
     console.error('Failed to retrieve video source.', err)
