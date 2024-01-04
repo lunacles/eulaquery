cde5f02d995646f60703d6771446955a1a3e170a
damocles <lunahackley@gmail.com>
2024-01-04 00:32:48 -0600

    Create options.js

---
+++ public/menus/options.js
@@ -0,0 +1,79 @@
+import global from '../global.js'
+import * as util from '../util.js'
+import Storage from '../localstorage.js'
+
+import {
+  Rect,
+  Text,
+  Line,
+} from '../elements.js'
+import Menu from '../components/menu.js'
+import Toggle from '../components/toggle.js'
+import MenuButton from '../components/menubutton.js'
+
+export let toggles = [{
+  label: 'Save Session Tags',
+  toggle: Toggle.create(global.options.saveTags, state => {
+    global.options.saveTags = state
+    Storage.options.saveTags.set({
+      value: state,
+    })
+  })
+}, {
+  label: 'Snow Fall',
+  toggle: Toggle.create(global.ui.snowFall, state => {
+    global.ui.snowFall = state
+    Storage.ui.snowFall.set({
+      value: state,
+    })
+  })
+}]
+
+export const OptionsButton = MenuButton.create('arrow')
+export const OptionsMenu = Menu.create({
+  button: OptionsButton,
+  elementSpacing: 15,
+}).background((x, y, width, height) => {
+  Rect.draw({
+    x, y,
+    width, height
+  }).both(util.mixColors(global.colors.black, global.colors.white, 0.025), util.mixColors(global.colors.black, global.colors.white, 0.2), 4)
+}).seperator((x, y, width, height) => {
+  Line.draw({
+    x1: x + width * 0.1, y1: y + height * 0.5,
+    x2: x + width, y2: y + height * 0.5,
+  }).alpha(0.5).stroke(util.mixColors(global.colors.black, global.colors.white, 0.2), 2)
+}).appendZone((x, y, width, height) => {
+  let spacing = 5
+  let size = util.fitTextToArea({
+    text: 'Content Filter',
+    width: width - spacing * 2,
+    height: height - spacing * 2,
+  })
+
+  Text.draw({
+    x: x + spacing, y: y + size * 1.1,
+    size,
+    text: 'Content Filter',
+    align: 'left',
+  }).fill(global.colors.white)
+})
+  // Options
+.appendZone((x, y, width, height) => {
+  let spacing = 15
+  let toggleWidth = width * 0.1
+  let toggleHeight = height / toggles.length - spacing
+
+  for (let [i, { label, toggle }] of toggles.entries()) {
+    toggle.draw({
+      x: x + spacing + toggleWidth * 0.7, y: y - spacing * 0.25 + spacing * i + toggleHeight * (i + 1),
+      width: toggleWidth, height: toggleHeight,
+    })
+    Text.draw({
+      x: x + toggleWidth * 2 + spacing * 2, y: y + spacing * i + toggleHeight * (i + 1),
+      size: toggleHeight * 0.5,
+      align: 'left',
+      text: label
+    }).fill(global.colors.white)
+  }
+})
