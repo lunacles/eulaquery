43cb49c30693baba073821124ed00f932a4a5d6a
damocles <lunahackley@gmail.com>
2024-01-04 00:32:36 -0600

    Create main.js

---
+++ public/menus/main.js
@@ -0,0 +1,105 @@
+import global from '../global.js'
+import Document from '../document.js'
+import * as util from '../util.js'
+
+import {
+  Rect,
+  Text,
+  Line,
+} from '../elements.js'
+import Menu from '../components/menu.js'
+import MenuButton from '../components/menubutton.js'
+
+import { ContentFilterButton } from './contentfilter.js'
+import { OptionsButton } from './options.js'
+
+export const MainMenuButton = MenuButton.create('hamburger')
+export const MainMenu = Menu.create({
+  button: MainMenuButton,
+  elementSpacing: 15,
+}).background((x, y, width, height) => {
+  Rect.draw({
+    x, y,
+    width, height
+  }).both(global.colors.black, util.mixColors(global.colors.black, global.colors.white, 0.2), 4)
+}).seperator((x, y, width, height) => {
+  Line.draw({
+    x1: x, y1: y + height * 0.5,
+    x2: x + width * 0.9, y2: y + height * 0.5,
+  }).alpha(0.5).stroke(util.mixColors(global.colors.black, global.colors.white, 0.2), 2)
+})
+
+// Title
+.appendZone((x, y, width, height) => {
+  let spacing = 5
+  let offset = Document.height * 0.05 * 0.85 + spacing * 2.5
+  let size = util.fitTextToArea({
+    text: 'Eulaquery',
+    width: width - offset - spacing,
+    height: height - spacing * 2,
+  })
+
+  Text.draw({
+    x: x + offset, y: y + size * 1.1,
+    size: size,
+    text: 'Eula',
+    align: 'left',
+  }).fill(global.colors.lightBlue)
+
+  let eulaTextWidth = util.measureText('Eula', size).width
+  Text.draw({
+    x: x + offset + eulaTextWidth, y: y + size * 1.1,
+    size: size,
+    text: 'query',
+    align: 'left'
+  }).fill(global.colors.burple)
+})
+
+// Content Filter Header
+.appendZone((x, y, width, height) => {
+  let spacing = 5
+  let size = util.fitTextToArea({
+    text: 'Content Filter',
+    width: width - spacing,
+    height: height - spacing * 2,
+  })
+
+  Text.draw({
+    x: x + spacing, y: y + size * 1.1,
+    size,
+    text: 'Content Filter',
+    align: 'left',
+  }).fill(global.colors.white)
+
+  ContentFilterButton.draw({
+    x: x + width - spacing - size, y: y + spacing,
+    width: size, height: size,
+  })
+  if (ContentFilterButton.state) {
+    OptionsButton.state = false
+  }
+})
+// Options Header
+.appendZone((x, y, width, height) => {
+  let spacing = 5
+  let size = util.fitTextToArea({
+    text: 'Options',
+    width: width - spacing,
+    height: height - spacing * 2,
+  })
+
+  Text.draw({
+    x: x + spacing, y: y + size * 1.1,
+    size,
+    text: 'Options',
+    align: 'left',
+  }).fill(global.colors.white)
+
+  OptionsButton.draw({
+    x: x + width - spacing - size, y: y + spacing,
+    width: size, height: size,
+  })
+  if (OptionsButton.state) {
+    ContentFilterButton.state = false
+  }
+})
