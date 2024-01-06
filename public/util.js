a044934d7f9bdd60c98a09d16dae19060498d9d1
damocles <lunahackley@gmail.com>
2024-01-05 23:48:56 -0600

    Add date formating

--- public/util.js
+++ public/util.js
@@ -31,3 +31,28 @@ export const fitTextToArea = ({ text = '', width = 0, height = 0 }) => {
   let maxSizeWidth = width / aspectRatio
   return Math.min(maxSizeWidth, height)
 }
+
+let getSuffix = day => {
+  let last = day % 10
+  let lastTwo = day % 100
+  if ([11, 12, 13].includes(lastTwo)) return 'th'
+  switch (last) {
+    case 1: return 'st'
+    case 2: return 'nd'
+    case 3: return 'rd'
+    default: return 'th'
+  }
+}
+
+export const formatDate = string => {
+  let date = new Date(string)
+  try {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    if (!(date instanceof Date && !isNaN(date))) throw new Error('Invalid date')
+    let day = date.getUTCDate()
+    let name = months[date.getUTCMonth()]
+    return `${name} ${day}${getSuffix(day)}, ${date.getUTCFullYear()}`
+  } catch (err) {
+    console.error('Failed to retrieve date:', err)
+  }
+}
