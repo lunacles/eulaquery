a229cddc124bfadeaad6a4efa48e3eed339edc6b
damocles <lunahackley@gmail.com>
2024-01-05 23:48:21 -0600

    Create repo.js

---
+++ public/repo.js
@@ -0,0 +1,26 @@
+import * as util from './util.js'
+
+const Build = {
+  id: 'unknown',
+  date: 0,
+  message: '',
+  diff: '',
+  async load() {
+    try {
+      let response = await fetch('https://api.github.com/repos/spanksmcyeet/eulaquery/commits/main')
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`)
+      }
+      let json = await response.json()
+      Build.id = json.sha.slice(0, 7)
+      Build.message = json.commit.message
+      Build.diff = json.html_url
+      Build.date = util.formatDate(json.commit.author.date)
+    } catch (err) {
+      console.error('Error loading commit data:', err)
+    }
+  },
+}
+Build.load()
+
+export default Build
