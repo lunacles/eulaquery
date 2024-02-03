553e4263f9f825b0e44a48870664f271c3b0f20b
damocles <lunahackley@gmail.com>
2024-02-02 17:20:32 -0800

    Make Button position from top left

--- public/components/button.js
+++ public/components/button.js
@@ -1,9 +1,10 @@
 import {
   Element,
 } from '../elements.js'
-import Interaction from '../interaction.js'
 
 import ClickRegion from './clickregion.js'
+
+import Interaction from '../interaction.js'
 
 const Button = class extends Element {
   static create({ defaultState = false, onUpdate = () => {} } = {}) {
@@ -14,7 +15,7 @@ const Button = class extends Element {
 
     this.state = defaultState
     this.onUpdate = onUpdate
-    
+
     this.x = 0
     this.y = 0
     this.width = 0
@@ -29,8 +30,8 @@ const Button = class extends Element {
     this.height = height
 
     this.clickRegion.update({
-      x: this.x - this.width * 0.5, y: this.y - this.height * 0.5,
-      width: this.width * 2, height: this.height * 2,
+      x: this.x, y: this.y,
+      width: this.width, height: this.height,
     })
 
     if (this.clickRegion.check() && Interaction.mouse.left) {
