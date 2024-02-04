3386847178aed619b6c47e9449fcbc0edefb8bbb
damocles <lunahackley@gmail.com>
2024-02-03 18:27:31 -0800

    Make promise & remove debug logging

--- public/processor.js
+++ public/processor.js
@@ -1,30 +1,30 @@
 import Profiler from './profiler.js'
 import global from './global.js'
+import Log from './log.js'
 
 // Fuck CORS
 const processor = async (src) => {
-  Profiler.logs.processor.set()
-  try {
+  return new Promise(async (resolve, reject) => {
+    Profiler.logs.processor.set()
     let response = await fetch(global.server.to, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ body: 'process', minWidth: global.api.postWidth, url: src.replace(/api-cdn(-mp4)?/g, global.api.server) })
+      body: JSON.stringify({ 
+        body: 'process', 
+        minWidth: global.api.postWidth, 
+        url: src.replace(/api-cdn(-mp4)?/g, global.api.server),
+      })
     })
+    if (!response.ok) reject(`HTTP error: ${response.status}`)
     let blob = await response.blob()
     let url = URL.createObjectURL(blob)
     let buffer = await blob.arrayBuffer().then(arrayBuffer => new Uint8Array(arrayBuffer))
     Profiler.logs.processor.mark()
 
-    if (global.debug)
-      console.log('Processor time:', `${Profiler.logs.processor.sum()}ms`)
-
-    return { url, buffer }
-  } catch (err) {
-    console.error('Failed to retrieve video source.', err)
-    return null
-  }
+    resolve({ url, buffer })
+  })
 }
 
 export default processor
