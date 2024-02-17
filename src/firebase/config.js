bacfcb02971cac681c2120b3fc7aa8204ff856d9
lunacles <lunahackley@gmail.com>
2024-02-17 08:02:27 -0600

    Add firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: 'AIzaSyAhNOJATgsfkOUgYCCEZ6yjQbBEN0qIius',
+  authDomain: 'eulaquery.firebaseapp.com',
+  projectId: 'eulaquery',
+  storageBucket: 'eulaquery.appspot.com',
+  messagingSenderId: '828974404029',
+  appId: '1:828974404029:web:070962f992b23713b402d4',
+  measurementId: 'G-LTGS68TL38'
+}
+
+export default config
